d509a10e0d34b6a80a32c85cdfdeae2e918a9579
Tummalapalli Madhuri <madhuri.tummalapalli96@gmail.com>
2016-03-22 18:30:58 +0530

    Add Dataset and Code for Parsing Titles

---
+++ code/parseData.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+from tokenizer import *
+from stemmer import *
+
+paperIdRe = re.compile(ur"id = \{(.*?)\}", flags = re.U)
+titleRe = re.compile(ur"title = \{(.*?)\}", flags = re.U)
+yearRe = re.compile(ur"year = \{(.*?)[\n]*\}", flags = re.U)
+
+
+def processTitles(titles):
+    for ind, title in enumerate(titles):
+        title = tokenize(title)
+        titles[ind] = stemList(title)
+
+    return titles
+
+
+def parse(dataFile):
+    data = open(dataFile).read()
+
+    ids = paperIdRe.findall(data)
+    titles = titleRe.findall(data)
+    years = yearRe.findall(data)
+
+    titles = processTitles(titles)
+    # Is any proccesing for 'year' required?
+    
+    return map(lambda x, y, z: (x, y, z), ids, titles, years)[:5000]
+
+if __name__ == "__main__":
+    dataFile = "../aan/release/2013/acl-metadata.txt"
+
+    parsedData = parse(dataFile)
